6d5bf2e7eba89473c5ab96ab4ce21182fce6f2a7
Michael_Simard <Mick.simard@gmail.com>
2022-11-24 13:25:53 -0500

    manque juste poisson
    probleme avec les poissons quand on les prends

---
+++ Assets/GravityChange.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityChange : MonoBehaviour
+{
+    public Vector3 gravity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity = gravity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
